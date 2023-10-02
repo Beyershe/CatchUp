20b7f9f27e3fa54cf50215d0f25423b6a6bb1644
Beyershe <133257978+Beyershe@users.noreply.github.com>
2023-10-02 13:22:51 -0400

    Scenes/Movement/Variables/Transforms

--- Assets/Scripts/NetworkHandler.cs
+++ Assets/Scripts/NetworkHandler.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.Netcode;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class NetworkHandler : NetworkBehaviour
